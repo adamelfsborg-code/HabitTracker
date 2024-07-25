2def1016ea19d841c412e0c70dbf31840b2ef700
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-25 16:33:37 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Adam Elfsborg on 2024-07-25.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
